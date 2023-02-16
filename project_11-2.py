eed7b6caf5739e07dfbb8d7b6e274e973b3144a9
jrmn-commits <113871835+jrmn-commits@users.noreply.github.com>
2023-02-16 17:12:13 -0500

    Finished Arrival Time Estimator

--- project_11-2.py
+++ project_11-2.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 #Project 11-2
 
-from datetime import date, time, datetime
+from datetime import date, time, datetime, timedelta
 
 #display message
 print("Arrival Time Estimator")
@@ -23,13 +23,27 @@ while continue_var == "y":
         
         dep_str= dep_date_str + " " + dep_time_str
         dep_datetime = datetime.strptime(dep_str, "%Y-%m-%d %I:%M %p")
-        hours = round(miles / miles_per_hr, 1)
+        time_traveled = float(miles / miles_per_hr) * 3600
         
+
+        #calc arrival datetime
+        arrival_datetime = dep_datetime + timedelta(seconds = time_traveled)
+        date_format = "%Y-%m-%d"
+        time_format = "%I:%M %p"
+        travel_span = (arrival_datetime - dep_datetime).seconds
+        arrival_timespan = travel_span // 60
+        arrival_hr = arrival_timespan // 60
+        arrival_mins = arrival_timespan % 60
+
+        #print results
         print("Estimated travel time")
-        print("Hours:\t", hours)
+        print("Hours:\t\t", arrival_hr)
+        print("Minutes:\t", arrival_mins)
+        print(f"Estimated date of arrival:\t {arrival_datetime:{date_format}}")
+        print(f"Estimated date of arrival:\t {arrival_datetime:{time_format}}")
         
         continue_var = input("Continue? (y/n): ") #loop check
-        print("===============================================================")
+        print()
         if continue_var == "y": #if y then begin loop / if n then stop and print
             continue
         else:
